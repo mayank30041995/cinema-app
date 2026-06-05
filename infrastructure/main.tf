de8fec58702097de8e45863411148bf25468e2cf
Mayank Sharma <mayankece155@gmail.com>
2026-06-05 16:56:49 +0530

    Working staging deployment on Node 20 and AWS

---
+++ infrastructure/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "mayank-cinema-tf-state-930767549449"
+    key     = "app-cinema.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
+  }
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    ManageBy    = "Terraform"
+    Owner = "Mayank"
+  }
+}
+
