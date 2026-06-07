febad66af2a8a84859c7051069213cedd55bcec3
Mayank Sharma <mayankece155@gmail.com>
2026-06-07 11:24:41 +0530

    rollback changes and finalize cicd proper

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -154,7 +154,6 @@ const Header = (props) => {
           <div className="header-navbar">
             <div className="header-image" onClick={() => navigateToMainPage()}>
               <img src={logo} alt="" />
-              <img src={logo} alt="" />
             </div>
             <div
               className={`${menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'}`}
